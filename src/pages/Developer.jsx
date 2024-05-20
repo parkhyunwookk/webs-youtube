da755d092c0e3010b578a42988c4a3eda208f9b1
parkhyunwookk <pol1017@naver.com>
2024-05-20 15:51:36 +0900

    페이지 만들기

---
+++ src/pages/Developer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Deverloper = () => {
+  return <div>Deverloper</div>;
+};
+
+export default Deverloper;
---
+++ src/pages/Youtebe.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Youtebe = () => {
+  return <div>Youtebe</div>;
+};
+
+export default Youtebe;
